a3d5a3b59c04eb8e8e819c7edf479223b12c6609
Ittipol k <Ittipol.Kaewprasert@krungsri.com>
2023-06-05 15:49:12 +0700

    add .gitignore to ignore build folder

--- server/build/mongoose/user.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const User = new mongoose_1.Schema({
-    firstname: String,
-    lastname: String,
-    age: Number
-});
-exports.default = (0, mongoose_1.model)("User", User);
